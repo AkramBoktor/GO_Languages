da082c2569b0e9d58fe51b14a700927218930eee
Akram Boktor <akram.boktor2013@gmail.com>
2022-07-04 07:11:44 +0200

    defer and switch case statment

--- FlowControlStatment.go
+++ FlowControlStatment.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"runtime"
 )
 
 func main() {
@@ -27,4 +29,49 @@ func main() {
 			fmt.Printf("The value of condition if is %v", i)
 		}
 	}
+
+	// if statment shorcut
+	fmt.Printf("shortcut of using if %v \n", Pow(3, 3, 20))
+	fmt.Printf("shortcut of using if %v \n", Pow(3, 2, 10))
+
+	// switch case
+	/*
+	 the break statement that is needed at the end of
+	 each case in those languages is provided automatically in Go.
+	 Another important difference is that Go's switch cases need not be constants,
+	 and the values involved need not be integers.
+	*/
+	switch os := runtime.GOOS; os {
+
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.\n", os)
+	}
+
+	// Stacking defers
+
+	/*
+		Stacking defers
+		Deferred function calls are pushed onto a stack.
+		When a function returns, its deferred calls are executed in last-in-first-out order.
+	*/
+	fmt.Println("First Print Counting")
+	//because it's like stack so it's will be last in first out
+	for count := 0; count < 5; count++ {
+		defer fmt.Println(count)
+	}
+
+	fmt.Println("Last Print")
+}
+
+func Pow(x float64, y float64, limit float64) float64 {
+	if v := math.Pow(x, y); v < limit {
+		return v
+	}
+	return limit
 }
