6545823bf0d3855d6e9ea2533f168f9e46c91179
Akram Boktor <akram.boktor2013@gmail.com>
2022-08-06 21:46:09 +0200

    array

--- Loops.go
+++ Loops.go
@@ -19,5 +19,11 @@ func main() {
 
 		arr[i] = i
 	}
-	fmt.Printf("Array values is %v", arr)
+	fmt.Printf("Array values is %v \n", arr)
+
+	fmt.Println("2D array Loops")
+
+	arr2d := [2][3]int{{1, 3, 4}, {5, 6, 7}}
+
+	fmt.Printf("Array 2d values is %v \n", arr2d[0][1])
 }
