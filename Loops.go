3b76ad15ffe6356b0ef481a55f077166cbb13253
Akram Boktor <akram.boktor2013@gmail.com>
2022-08-08 18:16:05 +0200

    Range and Get Duplicated Element

--- Loops.go
+++ Loops.go
@@ -26,4 +26,28 @@ func main() {
 	arr2d := [2][3]int{{1, 3, 4}, {5, 6, 7}}
 
 	fmt.Printf("Array 2d values is %v \n", arr2d[0][1])
+
+	fmt.Println("Range Element")
+	var arrayRange = [5]int{1, 3, 4, 5, 2}
+	for i, ele := range arrayRange {
+
+		fmt.Printf("index %d value %d \n", i, ele)
+	}
+	//anonymous variables
+
+	for _, ele := range arrayRange {
+
+		fmt.Printf(" value %d \n", ele)
+	}
+
+	//get duplicated element in the array
+	var arrayDuplicated = [7]int{1, 3, 2, 1, 3, 5, 6}
+	for i, element := range arrayDuplicated {
+
+		for j := i + 1; j < len(arrayDuplicated); j++ {
+			if element == arrayDuplicated[j] {
+				fmt.Printf("Duplicated Values is %d \n", element)
+			}
+		}
+	}
 }
