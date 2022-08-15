261db42c9b98417d7a7ac5090bb453ad92c1fee4
Akram Boktor <akram.boktor2013@gmail.com>
2022-08-15 20:35:24 +0200

    function called another function

--- Swap_Function_Return_MultipleResult.go
+++ Swap_Function_Return_MultipleResult.go
@@ -8,6 +8,9 @@ func main() {
 	a, b := swap("hello", "world")
 	fmt.Println(a, b)
 	fmt.Println(split(10))
+	// function calllent function
+	firstfunction(myfunct)
+
 }
 
 func swap(x, y string) (string, string) {
@@ -19,4 +22,11 @@ func split(num int) (x, y int) {
 	x = num + 10
 	y = num - 10
 	return
+}
+
+func firstfunction(myfunct func(int) int) {
+	fmt.Printf("Function callled function %v", myfunct(5))
+}
+func myfunct(x int) int {
+	return x
 }
